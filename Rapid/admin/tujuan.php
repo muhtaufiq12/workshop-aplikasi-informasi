25dcdbb0b045445075ca402c4979aa7971ab21c0
novarita milianti putri <novarita10.nr@gmail.com>
2020-04-13 09:41:30 +0700

    admin tujuan

--- Rapid/admin/tujuan.php
+++ Rapid/admin/tujuan.php
@@ -1,88 +1,141 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+{ 
+    header('location:login.php');
+}
+else{
+    if(isset($_POST['update']))
+    {
+        $pagetype='tujuan';
+        $pagetitle=$_POST['pagetitle'];
+        $pagedetails=$_POST['pagedescription'];
+
+        $query=mysqli_query($con,"update tblprofile set PageTitle='$pagetitle',Description='$pagedetails' where PageName='$pagetype' ");
+        if($query)
+        {
+            $msg="Tujuan successfully updated ";
+        }
+    else{
+        $error="Something went wrong . Please try again.";    
+    } 
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-        <meta name="author" content="Coderthemes">
+<head>
+    <meta charset="utf-8">
+    <title>Kelurahan Karah | Admin Panel</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
-        <!-- App title -->
-        <title>Kelurahan Karah | Sambutan Lurah</title>
+    <!-- Favicons -->
+    <link href="img/Logo-PKK.png" rel="icon">
+    <link href="img/Logo-PKK.png" rel="apple-touch-icon">
 
-        <!-- Summernote css -->
-        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+    <!-- Summernote css -->
+    <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
 
-        <!-- Select2 -->
-        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <!-- Select2 -->
+    <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
 
-        <!-- Jquery filer css -->
-        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
-        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+    <!-- Jquery filer css -->
+    <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+    <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
 
-        <!-- App css -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
-        <script src="assets/js/modernizr.min.js"></script>
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+    <script src="assets/js/modernizr.min.js"></script>
 
-    </head>
+</head>
 
 
-    <body class="fixed-left">
+<body class="fixed-left">
 
-        <!-- Begin page -->
-        <div id="wrapper">
+    <!-- Begin page -->
+    <div id="wrapper">
 
-            <!-- Top Bar Start -->
+        <!-- Top Bar Start -->
         <?php include('includes/topheader.php');?>
         <!-- ========== Left Sidebar Start ========== -->
         <?php include('includes/leftsidebar.php');?>
         <!-- Left Sidebar End -->
 
 
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="content-page">
-                <!-- Start content -->
-                <div class="content">
-                    <div class="container">
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="content-page">
+            <!-- Start content -->
+            <div class="content">
+                <div class="container">
 
 
-                        <div class="row">
-							<div class="col-xs-12">
-								<div class="page-title-box">
-                                    <h4 class="page-title">Tujuan PKK </h4>
-                                    <ol class="breadcrumb p-0 m-0">
-                                        <li>
-                                            <a href="#">Pages</a>
-                                        </li>
-                                     
-                                        <li class="active">
-                                         Profil
-                                        </li>
-                                    </ol>
-                                    <div class="clearfix"></div>
-                                </div>
-							</div>
-						</div>
-                        <!-- end row -->
+                    <div class="row">
+                     <div class="col-xs-12">
+                        <div class="page-title-box">
+                            <h4 class="page-title">Profil  </h4>
+                            <ol class="breadcrumb p-0 m-0">
+                                <li>
+                                    <a href="#">Pages</a>
+                                </li>
+                                
+                                <li class="active">
+                                   Visi Misi Karah
+                               </li>
+                           </ol>
+                           <div class="clearfix"></div>
+                       </div>
+                   </div>
+               </div>
+               <!-- end row -->
+
+               <div class="row">
+                <div class="col-sm-6">  
+<!---Success Message--->  
+<?php if($msg){ ?>
+<div class="alert alert-success" role="alert">
+<strong>Well done!</strong> <?php echo htmlentities($msg);?>
+</div>
+<?php } ?>
+
+<!---Error Message--->
+<?php if($error){ ?>
+<div class="alert alert-danger" role="alert">
+<strong>Oh snap!</strong> <?php echo htmlentities($error);?></div>
+<?php } ?>
+
+
+</div>
+</div>
+<?php 
+$pagetype='tujuan';
+$query=mysqli_query($con,"select PageTitle,Description from tblprofile where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
 
                         <div class="row">
                             <div class="col-md-10 col-md-offset-1">
                                 <div class="p-6">
                                     <div class="">
-                                        <form name="aboutus" method="post">
+                                        <form name="tujuan" method="post">
  <div class="form-group m-b-20">
 <label for="exampleInputEmail1">Page Title</label>
-<input type="text" class="form-control" id="pagetitle" name="pagetitle" value="Tujuan PKK"  required>
+<input type="text" class="form-control" id="pagetitle" name="pagetitle" value="<?php echo htmlentities($row['PageTitle'])?>"  required>
 </div>
 
 
@@ -94,13 +147,12 @@
      <div class="row">
 <div class="col-sm-12">
  <div class="card-box">
-<h4 class="m-b-30 m-t-0 header-title"><b>Page Detail</b></h4>
-<textarea class="summernote" name="pagedescription"  required><b>GERAKAN PKK BERTUJUAN MEMEBRDAYAKAN KELUARAGA UNTUK MENINGKATKAN KESEJAHTERAAN MENUJU TERWUJUDNYA KELUARGA YANG BERIMAN DAN BERTAQWA KEPADA TUHAN YANG MAHA ESA, BERAKHLAK MULIA DAN 
-            BERBUDI LUHUR, SEHAT SEJAHTERA, LAHIR DAN BATIN.</b></textarea>
+<h4 class="m-b-30 m-t-0 header-title"><b>Page Details</b></h4>
+<textarea class="summernote" name="pagedescription"  required><?php echo htmlentities($row['Description'])?></textarea>
 </div>
 </div>
 </div>
-
+<?php } ?>
 
 <button type="submit" name="update" class="btn btn-success waves-effect waves-light">Update and Post</button>
 
@@ -117,7 +169,7 @@
 
                 </div> <!-- content -->
 
-           <?php include('includes/footer.php');?>
+                <?php include('includes/footer.php');?>
 
             </div>
 
@@ -133,39 +185,39 @@
 
 
         <script>
-            var resizefunc = [];
-        </script>
+        var resizefunc = [];
+    </script>
 
-        <!-- jQuery  -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/detect.js"></script>
-        <script src="assets/js/fastclick.js"></script>
-        <script src="assets/js/jquery.blockUI.js"></script>
-        <script src="assets/js/waves.js"></script>
-        <script src="assets/js/jquery.slimscroll.js"></script>
-        <script src="assets/js/jquery.scrollTo.min.js"></script>
-        <script src="../plugins/switchery/switchery.min.js"></script>
+    <!-- jQuery  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/detect.js"></script>
+    <script src="assets/js/fastclick.js"></script>
+    <script src="assets/js/jquery.blockUI.js"></script>
+    <script src="assets/js/waves.js"></script>
+    <script src="assets/js/jquery.slimscroll.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="../plugins/switchery/switchery.min.js"></script>
 
-        <!--Summernote js-->
-        <script src="../plugins/summernote/summernote.min.js"></script>
-        <!-- Select 2 -->
-        <script src="../plugins/select2/js/select2.min.js"></script>
-        <!-- Jquery filer js -->
-        <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
+    <!--Summernote js-->
+    <script src="../plugins/summernote/summernote.min.js"></script>
+    <!-- Select 2 -->
+    <script src="../plugins/select2/js/select2.min.js"></script>
+    <!-- Jquery filer js -->
+    <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
 
-        <!-- page specific js -->
-        <script src="assets/pages/jquery.blog-add.init.js"></script>
+    <!-- page specific js -->
+    <script src="assets/pages/jquery.blog-add.init.js"></script>
 
-        <!-- App js -->
-        <script src="assets/js/jquery.core.js"></script>
-        <script src="assets/js/jquery.app.js"></script>
+    <!-- App js -->
+    <script src="assets/js/jquery.core.js"></script>
+    <script src="assets/js/jquery.app.js"></script>
 
-        <script>
+    <script>
 
-            jQuery(document).ready(function(){
+    jQuery(document).ready(function(){
 
-                $('.summernote').summernote({
+        $('.summernote').summernote({
                     height: 240,                 // set editor height
                     minHeight: null,             // set minimum height of editor
                     maxHeight: null,             // set maximum height of editor
@@ -179,13 +231,14 @@
                 });
             });
         </script>
-  <script src="../plugins/switchery/switchery.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
 
         <!--Summernote js-->
         <script src="../plugins/summernote/summernote.min.js"></script>
 
-    
+        
 
 
     </body>
-</html>
+    </html>
+    <?php } ?>
